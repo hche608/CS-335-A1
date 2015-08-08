5f474162589eb8d30d53e14b685c80ff1c533158
Hao CHEN <hao.ch86@gmail.com>
2015-08-08 14:16:07 +1200

    Completed funcs

--- A1/Program.cs
+++ A1/Program.cs
@@ -12,8 +12,10 @@ namespace A1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
             string loc_1, fname_or_url_1, xpath_1, kpath_1, spath_1, loc_2, fname_or_url_2, xpath_2, kpath_2, spath_2;
 
             loc_1 = args[0];
@@ -21,165 +23,84 @@ namespace A1
             xpath_1 = args[2];
             kpath_1 = args[3];
             spath_1 = args[4];
-            var xDocLeft = new XDocument();
+            XDocument xDocLeft = new XDocument();
 
             loc_2 = args[5];
             fname_or_url_2 = args[6];
             xpath_2 = args[7];
             kpath_2 = args[8];
             spath_2 = args[9];
-            var xDocRight = new XDocument();
-            /*
-            loc_1 = "/FILE-XML";
-            fname_or_url_1 = "MyCustomers.xml";
-            xpath_1 = "Customers/Customer";
-            kpath_1 = "@CustomerID";
-            spath_1 = "@CustomerID";
-            var xDocLeft = new XDocument();
+            XDocument xDocRight = new XDocument();
 
-            loc_2 = "/FILE-JSON";
-            fname_or_url_2 = "Orders.json";
-            xpath_2 = "root/value";
-            kpath_2 = "OrderID";
-            spath_2 = "OrderID";
-            var xDocRight = new XDocument();
-            */
-
-
-
-
-            switch (loc_1)
+            Func<String, XDocument> loadxml = delegate(String s)
             {
-                case "/FILE-JSON":
-                    // FILE-JSON ==> XML            
-                    try
-                    {
-                        var json_data = string.Empty;
-                        json_data = File.ReadAllText(fname_or_url_1);
-                        xDocLeft = JsonConvert.DeserializeXNode(json_data, "root");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Loading Json Data error: {0}\n\n", e);
-                    }
-                    break;
-                case "/URL-JSON":
-                    //xDocLeft = XDocument.Load(fname_or_url_1);
+                XDocument result = XDocument.Load(s);
+                return result;
+            };
 
-
-                    using (var w = new System.Net.WebClient())
-                    {
-                        string server = "http://services.odata.org/Northwind/Northwind.svc/";
-                        string order = "Orders()?$orderby=OrderID desc&$select=OrderID,CustomerID,EmployeeID";
-                        string format = "&$format=json";
-                        string url = server + order + format;
-                        var json_data = string.Empty;
-                        try
-                        {
-                            var fullXml = new XDocument();
-                            json_data = w.DownloadString(url);
-                            XDocument xml = Newtonsoft.Json.JsonConvert.DeserializeXNode(json_data, "root");
-
-                        nextLine:
-                            if (xml.XPathSelectElement("//odata.nextLink") != null)
-                            {
-                                
-                                url = server + xml.XPathSelectElement("//odata.nextLink").Value + format;
-                                xml.XPathSelectElement("//odata.nextLink").Remove();
-
-
-                                json_data = w.DownloadString(url);
-                                xml = Newtonsoft.Json.JsonConvert.DeserializeXNode(json_data, "root");
-
-
-                                goto nextLine;
-                            }
-
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine("Fetching Json Data error: {0}\n\n", e);
-                        }
-                    }
-
-                    Console.ReadKey();
-
-                    break;
-                default:
-                    xDocLeft = XDocument.Load(fname_or_url_1);
-                    break;
-            }
-
-            switch (loc_2)
+            Func<String, XDocument> loadjson = delegate(String s)
             {
-                case "/FILE-JSON":
-                    try
-                    {
-                        var json_data = string.Empty;
-                        json_data = File.ReadAllText(fname_or_url_2);
-                        xDocRight = (XDocument)JsonConvert.DeserializeXNode(json_data, "root");
-                        xDocRight.Save("JsonToXML.xml");
-                        Console.WriteLine("Loaded a Json file.\n");
-                        Console.WriteLine(xDocRight.XPathSelectElement("root/value").Name + "\n\n");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Loading Json Data error: {0}\n\n", e);
-                    }
-                    break;
-                case "/URL-JSON":
-                    //xDocRight = XDocument.Load(fname_or_url_2);
-                    break;
-                default:
-                    xDocRight = XDocument.Load(fname_or_url_2);
-                    break;
-            }
+                var json_data = string.Empty;
+                json_data = File.ReadAllText(s);
+                XDocument result = Newtonsoft.Json.JsonConvert.DeserializeXNode(json_data, "root");
+                return result;
+            };
 
+            Func<String, XDocument> loadurl = delegate(String s)
+            {
+                string server = @"http://services.odata.org/Northwind/Northwind.svc/";
+                string order = "Orders()?$orderby=OrderID desc&$select=OrderID,CustomerID,EmployeeID";
+                string format = "&$format=json";
+                string url = server + order + format;
+
+                List<XElement> nodes = new List<XElement>();
+                var w = new System.Net.WebClient();
+                XDocument xml = Newtonsoft.Json.JsonConvert.DeserializeXNode(w.DownloadString(url), "root");
+
+            nextLine:
+                if (xml.XPathSelectElement("//odata.nextLink") != null)
+                {
+                    url = server + xml.XPathSelectElement("//odata.nextLink").Value + format;
+                    xml.XPathSelectElement("//odata.nextLink").Remove();
+                    xml.XPathSelectElement("//odata.metadata").Remove();
+                    nodes.AddRange(xml.XPathSelectElements("root/value"));
+                    xml = Newtonsoft.Json.JsonConvert.DeserializeXNode(w.DownloadString(url), "root");
+                    goto nextLine;
+                }
+                nodes.AddRange(xml.XPathSelectElements("root/value"));
+                XDocument result = new XDocument(new XElement("root", nodes));
+                return result;
+            };
+
+            Func<String, String, String, XDocument, XDocument> orderByKey = delegate(String title, String xpath, String key, XDocument xDoc)
+            {
+                XDocument result = new XDocument();
+                if (key.Substring(0, 1) == "@")
+                    result = new XDocument(new XElement(title, xDoc.XPathSelectElements(xpath).OrderBy(atr => (String)atr.Attribute(key.Substring(1, key.Length - 1)).Value, StringComparer.Ordinal)));
+                else
+                    result = new XDocument(new XElement(title, xDoc.XPathSelectElements(xpath).OrderBy(el => (String)el.XPathSelectElement(key), StringComparer.Ordinal)));
+                result.Save("_" + title + ".xml");
+                return result;
+            };
+
+
+            if (loc_1 == "/FILE-JSON")
+                xDocLeft = loadjson(fname_or_url_1);
+            if (loc_2 == "/FILE-JSON")
+                xDocRight = loadjson(fname_or_url_2);
+            if (loc_1 == "/URL-JSON")
+                xDocLeft = loadurl(fname_or_url_1);
+            if (loc_2 == "/URL-JSON")
+                xDocRight = loadurl(fname_or_url_2);
+            if (loc_1 == "/FILE-XML")
+                xDocLeft = loadxml(fname_or_url_1);
+            if (loc_2 == "/FILE-XML")
+                xDocRight = loadxml(fname_or_url_2);
 
             // LeftSeqs
-            //var xDoc = XDocument.Load(fname_or_url_1);
-
-            var test = leftXDoc.XPathSelectElements(xpath_1).ToList().OrderBy(el => (String)el.XPathSelectElement(""), StringComparer.Ordinal);
-            IOrderedEnumerable<XElement> nodes;
-            switch (spath_1.Substring(0, 1))
-            {
-                case "@":
-                    nodes = from feed in xDocLeft.XPathSelectElements(xpath_1)
-                            orderby feed.Attribute(spath_1.Substring(1, spath_1.Length - 1)).Value
-                            select feed;
-                    break;
-                default:
-                    nodes = from feed in xDocLeft.XPathSelectElements(xpath_1)
-                            orderby feed.Value
-                            select feed;
-                    break;
-            }
-
-            var result_LeftSeqs = new XElement("LeftSeq", nodes);
-
-            //System.Console.Write(result_LeftSeqs);
-            result_LeftSeqs.Save("_LeftSeq.xml");
-
+            xDocLeft = orderByKey("LeftSeq", xpath_1, spath_1, xDocLeft);
             // RightSeqs
-            //xDoc = XDocument.Load(fname_or_url_2);
-            //IOrderedEnumerable<XElement> nodes;
-            switch (spath_2.Substring(0, 1))
-            {
-                case "@":
-                    nodes = from feed in xDocRight.XPathSelectElements(xpath_2)
-                            orderby feed.Attribute(spath_2.Substring(1, spath_2.Length - 1)).Value
-                            select feed;
-                    break;
-                default:
-                    nodes = from feed in xDocRight.XPathSelectElements(xpath_2)
-                            orderby feed.Value
-                            select feed;
-                    break;
-            }
-            var result_RightSeqs = new XElement("RightSeq", nodes);
-
-            //System.Console.Write(result_LeftSeqs);
-            result_RightSeqs.Save("_RightSeq.xml");
+            xDocRight = orderByKey("RightSeq", xpath_2, spath_2, xDocRight);
 
             /*
             try
