fa9f8dd5b57576a49b25a591f93d0c278029467f
Hao CHEN <hao.ch86@gamil.com>
2015-08-11 20:32:52 +1200

    Completed

--- A1/Program.cs
+++ A1/Program.cs
@@ -15,76 +15,67 @@ namespace A1
 
         static void Main(string[] args)
         {
-            Func<String, XDocument> loadxml = delegate (String s)
-            {
-                return XDocument.Load(s);
-            };
-
-            Func<String, XDocument> loadjson = delegate (String s)
-            {
-                var json_data = string.Empty;
-                json_data = File.ReadAllText(s);
-                return JsonConvert.DeserializeXNode(json_data, "root");
-            };
-
-            Action<String, String, String, List<XElement>> getUrlJson = null;
-            getUrlJson = delegate (String s, String o, String f, List<XElement> nodes)
-            {
-                var w = new WebClient();
-                XDocument xml;
-                try
-                {
-                    xml = JsonConvert.DeserializeXNode(w.DownloadString(s + o + f), "root");
-                    if (xml.XPathSelectElement("//odata.nextLink") != null)
-                    {
-                        o = xml.XPathSelectElement("//odata.nextLink").Value;
-                        xml.XPathSelectElement("//odata.nextLink").Remove();
-                        nodes.AddRange(xml.XPathSelectElements("root/value"));
-                        getUrlJson(s, o, f, nodes);
-                    }
-                    else
-                        nodes.AddRange(xml.XPathSelectElements("root/value"));
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("");
-                }
-            };
-
-            Func<String, XDocument> loadurl = delegate (String s)
-            {
-                string server = Regex.Match(s, @"(https?)(\w+|\W+)+?svc\/").Value;
-                string format = Regex.Match(s, @"\&\$format=\w+").Value;
-                string order = s.Replace(server, String.Empty).Replace(format, String.Empty);
-                List<XElement> nodes = new List<XElement>();
-                getUrlJson(server, order, format, nodes);
-                XDocument result = new XDocument(new XElement("root", nodes));
-                return result;
-            };
-
-            Func<XElement, String, String> selector = delegate (XElement x, String path)
-            {
-                var y = x.XPathEvaluate(path) as IEnumerable<object>;
-                if (y == null) return null;
-                dynamic z = y.FirstOrDefault();
-                if (z == null) return null;
-                return z.Value; // expected y : XElement or XAttribute
-            };
-
-            Func<String, String, String, XDocument, XDocument> orderByKey = delegate (String title, String xpath, String spath, XDocument xDoc)
-            {
-                XDocument result = new XDocument(new XElement(title, xDoc.XPathSelectElements(xpath).OrderBy(v => selector(v, spath), StringComparer.Ordinal)));
-                result.Save("_" + title + ".xml");
-                return result;
-            };
-
-            string loc_1, fname_or_url_1, xpath_1, kpath_1, spath_1, loc_2, fname_or_url_2, xpath_2, kpath_2, spath_2 = String.Empty;
-            XDocument xDocLeft = new XDocument();
-            XDocument xDocRight = new XDocument();
             try
             {
-                if (args.Count() < 9)
-                    throw new ArgumentException("too few command line arguments");
+                Func<String, XDocument> loadxml = s =>
+                {
+                    return XDocument.Load(s);
+                };
+
+                Func<String, XDocument> loadjson = s =>
+                {
+                    var json_data = string.Empty;
+                    json_data = File.ReadAllText(s);
+                    return JsonConvert.DeserializeXNode(json_data, "root");
+                };
+
+                Action<String, String, String, List<XElement>> getUrlJson = null;
+                getUrlJson = (s, o, f, nodes) =>
+            {
+                var w = new WebClient();
+                XDocument xml = JsonConvert.DeserializeXNode(w.DownloadString(new Uri(s + o + f)), "root");
+                if (xml.XPathSelectElement("//odata.nextLink") != null)
+                {
+                    o = xml.XPathSelectElement("//odata.nextLink").Value;
+                    xml.XPathSelectElement("//odata.nextLink").Remove();
+                    nodes.AddRange(xml.XPathSelectElements("root/value"));
+                    getUrlJson(s, o, f, nodes);
+                }
+                else
+                    nodes.AddRange(xml.XPathSelectElements("root/value"));
+            };
+
+                Func<String, XDocument> loadurl = delegate(String s)
+                {
+                    string server = Regex.Match(s, @"(https?)(\w+|\W+)+?svc\/").Value;
+                    string format = Regex.Match(s, @"\&\$format=\w+").Value;
+                    string order = s.Replace(server, String.Empty).Replace(format, String.Empty);
+                    List<XElement> nodes = new List<XElement>();
+                    getUrlJson(server, order, format, nodes);
+                    XDocument result = new XDocument(new XElement("root", nodes));
+                    return result;
+                };
+
+                Func<XElement, String, String> selector = (x, path) =>
+                {
+                    var y = x.XPathEvaluate(path) as IEnumerable<object>;
+                    if (y == null) return null;
+                    dynamic z = y.FirstOrDefault();
+                    if (z == null) return null;
+                    return z.Value; // expected y : XElement or XAttribute
+                };
+
+                Func<String, String, String, XDocument, XDocument> orderByKey = (title, xpath, spath, xDoc) =>
+                {
+                    XDocument result = new XDocument(new XElement(title, xDoc.XPathSelectElements(xpath).OrderBy(v => selector(v, spath), StringComparer.Ordinal)));
+                    result.Save("_" + title + ".xml");
+                    return result;
+                };
+
+                string loc_1, fname_or_url_1, xpath_1, kpath_1, spath_1, loc_2, fname_or_url_2, xpath_2, kpath_2, spath_2;
+                XDocument xDocLeft = new XDocument();
+                XDocument xDocRight = new XDocument();
+
                 loc_1 = args[0];
                 fname_or_url_1 = args[1];
                 xpath_1 = args[2];
@@ -103,24 +94,19 @@ namespace A1
                     xDocLeft = loadurl(fname_or_url_1);
                 else if (loc_1 == "/FILE-XML")
                     xDocLeft = loadxml(fname_or_url_1);
-                else
-                    throw new FileNotFoundException("left file wrong location");
 
                 if (loc_2 == "/FILE-JSON")
                     xDocRight = loadjson(fname_or_url_2);
-
                 else if (loc_2 == "/URL-JSON")
                     xDocRight = loadurl(fname_or_url_2);
-
                 else if (loc_2 == "/FILE-XML")
                     xDocRight = loadxml(fname_or_url_2);
-                else
-                    throw new FileNotFoundException("right file wrong location");
 
                 // LeftSeqs
                 xDocLeft = orderByKey("LeftSeq", xpath_1, spath_1, xDocLeft);
                 // RightSeqs
                 xDocRight = orderByKey("RightSeq", xpath_2, spath_2, xDocRight);
+
                 // Inner Join
                 XDocument result_InnerJoin = new XDocument(new XElement("InnerJoin",
                     from lSide in xDocLeft.XPathSelectElements("LeftSeq/*")
@@ -148,9 +134,8 @@ namespace A1
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                Console.Error.WriteLine(e);
             }
-
         }
     }
 }
---
+++ A1/README.txt
@@ -0,0 +1,10 @@
+CS335 Assignment 1
+Load XML 		Done
+Load JSON		Done
+Load URL		Done
+LeftSeq			Done
+RightSeq		Done
+InnerJoin		Done
+GroupJoin		Done
+LeftOuterJoin	Done
+All Execeptions Done
